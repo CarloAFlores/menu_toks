30be9e4350902315bfe0ea051349a6e56a02ded7
unknown <carlos2000sistemasc@gmail.com>
2023-01-04 11:23:54 -0800

    menu tks

---
+++ src/components/ModalCiudad.vue
@@ -0,0 +1,70 @@
+<template>
+   <div class="modal" v-if="confirmar" v-bind="Modal">
+    <form>
+        <p><img src="@/assets/logo.png" alt=""> Selecciona tu ciudad</p>
+        <select name="Ciudad" id="">
+            <option value="">Puebla</option>
+            <option value="">Cdmx</option>
+            <option value="">Monterrey</option>
+            
+        </select>
+    </form>
+    <div class="botones">
+    <button @click="confirmar=false">Si</button>
+    <button @click="confirmar=false">No</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            confirmar:true
+    }
+  },
+  props:['Modal']
+
+}
+</script>
+
+<style scoped>
+.modal{
+    background: gray;
+    height: 200px;
+    width: 500px;
+    border-radius: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Ubuntu', sans-serif;
+
+}
+
+select{
+    margin-left: 30px;
+}
+.botones{
+    margin-top: 30px;
+    }
+button{
+    margin-left: 30px;
+    background-color: orangered;
+    border: none;
+    width: 150px;
+    border-radius: 20px;
+    font-family: 'Ubuntu', sans-serif;
+}    
+
+@media screen and (max-width:500px){
+    button{
+        width: 90px;
+    }
+}
+img{
+    width: 20px;
+    height: 20px;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,58 @@
 <template>
+  <ModalCiudad class="modal" />
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Estar aqui es mejor</h2>
+    <router-link :to="{name:'about'}" class="Boton">MENU</router-link>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import ModalCiudad from "@/components/ModalCiudad.vue";
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {ModalCiudad},
 }
 </script>
+
+<style scoped>
+.home{
+  background-image: url('@/assets/Fondo.jpg');
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 700px;
+  font-family: 'Ubuntu', sans-serif;
+
+}
+
+.modal{
+  position: fixed;
+  margin-left: 30%;
+  margin-top: 20%;
+/*  */
+}
+.Boton{
+  text-decoration: none;
+  background-color: rgb(216, 118, 7);
+  color: white;
+  padding: 20px;
+  border-radius: 20px;
+  font-family: 'Ubuntu', sans-serif;
+
+}
+@media screen and (max-width:500px){
+  .home{
+    background-image: url('@/assets/Fondo.jpg');
+    background-size: 500px 890px;
+    background-repeat: no-repeat;
+  }
+  .modal{
+    width: 300px;
+    height: 200px;
+    margin-left: 13%;
+    margin-top: 50%;
+  }
+}
+</style>
